474943fec11f81b7747768dca23fc22977bc4e97
Rafiul Islam <rafiulislam4342@gmail.com>
2023-06-16 18:13:29 +0200

    first commit

---
+++ GetInteractionElement.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BaSyx.AAS.Client.Http;
+using BaSyx.Models.Core.AssetAdministrationShell;
+using BaSyx.Models.Core.AssetAdministrationShell.Generics;
+using BaSyx.Models.Core.AssetAdministrationShell.Implementations;
+using BaSyx.Models.Core.Common;
+using BaSyx.Models.Extensions;
+using BaSyx.Utils.ResultHandling;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace I40test
+{
+  
+    public class GetInteractionElement
+    {
+        public ISubmodelElement MaintenanceOrderDescription { get; private set; }
+
+        public SubmodelElementCollection GetSubmodelElements(string uri, string SubmodelIDShort, string SubmodelElementIDShort)
+        {
+
+            try
+            {
+
+                AssetAdministrationShellHttpClient client = new AssetAdministrationShellHttpClient(new Uri(uri));
+
+                Console.WriteLine(client);
+
+                var result = client.RetrieveAssetAdministrationShell();
+                Console.WriteLine(result.Entity.GetType());
+                if (result == null)
+                {
+                    Console.WriteLine("AAS data not received");
+                    Console.WriteLine(result.GetType());
+                }
+                //var a = result.Entity[]
+                else
+                {
+                    var AASGetData = result.Entity.ToJson();
+              //      Console.WriteLine(AASGetData);
+
+
+                    //   client.RetrieveSubmodelElementValue("Maintenence")
+                 //   Console.WriteLine(result);
+                   
+                }
+            var result1 = client.RetrieveSubmodelElement(SubmodelIDShort, SubmodelElementIDShort);
+             Console.WriteLine(result1.ToString());
+                 var result1Josn = result1.Entity.ToJson();
+                Console.WriteLine(result1Josn);
+                Console.WriteLine(result1.Entity.GetType());
+
+
+                //  var interactionElement = result1.Entity.Parent;
+                // Console.WriteLine(interactionElement);
+
+                //   var jsonData = (JObject)JsonConvert.DeserializeObject(result1Josn);
+                //  var message = jsonData["MaintenanceOrderDescription"];
+
+                //  Console.WriteLine(message);
+
+
+
+                /* JsonDocument doc = JsonDocument.Parse(result1Josn);
+
+                  JsonElement idShortElement = doc.RootElement
+                  .GetProperty("value")
+                  .EnumerateArray()
+                  .FirstOrDefault(x => x.GetProperty("idShort").GetString() == "MaintenanceOrderDescription")?
+                  .GetProperty("value");
+                  */
+
+                // Console.WriteLine(idShortElement.ToString());
+
+
+                //  JObject obj = JObject.Parse(result1Josn);
+
+                //JToken Orderdescription = obj["value"]
+                //.FirstOrDefault(x => x["idShort"].ToString() == "MaintenanceOrderDescription")?["value"];
+
+                //    Console.WriteLine(Orderdescription);
+
+                //var MHsev = se3.value.Find(n => n.idShort == "MaintenanceHistory");
+                //var mr1 = MHsev.value.Find(n => n.idShort == "MaintenanceRecord_1");
+                //Console.WriteLine(mr1.value is JsonElement);
+                //Console.WriteLine(mr1.value.ToString());
+
+
+                SubmodelElementCollection submodelElementCollection = JsonConvert.DeserializeObject<SubmodelElementCollection>(result1Josn);
+
+                Console.WriteLine(submodelElementCollection);
+
+                foreach (var element in submodelElementCollection.Value)
+                {
+                   IValue val = element.GetValue();
+                    Console.WriteLine(val.Value);
+                }
+
+
+                return submodelElementCollection;
+            }
+            catch(Exception ex)
+            {
+               
+                    Console.WriteLine(ex);
+                return null;
+            }
+
+
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using I40Extension.UtilsMqtt;
+using I40Extension.Message;
+using System.Collections.Generic;
+using BaSyx.Models.Core.AssetAdministrationShell;
+using BaSyx.Models.Core.AssetAdministrationShell.Implementations;
+using System.Text;
+using System.Net.Http;
+using BaSyx.AAS.Client.Http;
+using BaSyx.Models.Core.AssetAdministrationShell.Generics;
+using BaSyx.Models.Core.Common;
+using BaSyx.Models.Extensions;
+
+namespace I40test
+{
+    class Program
+    {
+        public static List<SubmodelElementCollection> inter = new List<SubmodelElementCollection>();
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // intantiate client
+
+            var mqttClient = new MqttClientWrapper("test.mosquitto.org", 1883);
+            string uri = "http://localhost:5180";
+            //Create message
+           GetInteractionElement ie = new GetInteractionElement();
+           SubmodelElementCollection orderdescription= ie.GetSubmodelElements(uri, "MaintenanceSubmodel", "Maintenance_1/MaintenanceOrderDescription");
+
+            
+            
+       
+
+            if (orderdescription != null)
+            {
+                foreach (var element in orderdescription.Value)
+            {
+                    if(element.IdShort == "MaintenanceElement")
+                    {
+                        IValue valuuu = element.GetValue();
+                        Console.WriteLine(valuuu);
+                        Console.WriteLine(element.IdShort);
+                    }
+               
+            }
+            }
+
+            inter.Add(orderdescription);
+
+
+            I40Extension.I40Message notify_init = new I40Extension.I40Message();
+
+            notify_init.frame = new I40Extension.Frame()
+            {
+                conversationId = "adsadasf",
+                messageId = "1",
+                sender =  {
+                    identification =
+                          {
+                    id = "BASYX_MACHINE_AAS_POC",
+                    idType = "Coustom",
+
+                },
+                     role =
+                            {
+                              name = "InformationSender",
+
+                          }
+                        },
+               
+                     receiver =
+                      {
+                          identification =
+                          {
+                              id ="MES_AAS",
+                              idType = "Custom",
+                          },
+                          role ={
+                                  name = "InformationReceiver"
+                          }
+              
+                      },
+                     type = "NOTIFY_INIT",
+
+                inReplyTO = "null", 
+                replyBy = "null", 
+                semanticProtocol =
+                {
+                     keys = new List<Key>
+                      {
+                          new Key
+                          {
+                              type = "GlobalReference",
+                              idType ="CUSTOM",
+                              value ="Maintenance"
+                          }
+                      },
+                }     
+            
+
+            };
+
+            notify_init.interactionElements = inter;
+
+            Console.WriteLine(notify_init.GetFrame());
+
+
+            /*
+              I40Extension.I40Message mes = new I40Extension.I40Message
+              {
+                 interactionElements = inter,
+
+                  frame =
+                  {
+                       type = "NOTIFY_INIT",
+                       sender =
+                      {
+                          identification =
+                          {
+                              id="BASYX_MACHINE_AAS_POC",
+                              idType="Coustom",
+
+                          },
+                          role =
+                          {
+                              name = "InformationSender",
+
+                          }
+                      },
+                       receiver =
+                      {
+                          identification =
+                          {
+                              id ="MES_AAS",
+                              idType = "Custom",
+                          },
+                          role ={
+                                  name = "InformationReceiver"
+                          }
+                      },
+                       conversationId ="Maintenance_1::1",
+                       messageId ="1",
+                      inReplyTO ="null",
+                      replyBy = "null",
+                      semanticProtocol = new SemanticProtocol
+                      {
+
+                      keys = new List<Key>
+                      {
+                          new Key
+                          {
+                              type = "GlobalReference",
+                              idType ="CUSTOM",
+                              value ="Maintenance"
+                          }
+                      }
+                      }
+                  }
+
+              };
+
+              var result = mqttClient.PublishAsync("Test", mes);
+              mqttClient.Subscribe("Test");
+
+              // mqtt Received message handler 
+              mqttClient.MessageReceived += (sender, args) => {
+
+                  var receivedTopics = args.ApplicationMessage.Topic;
+                  var receivedPayload = Encoding.UTF8.GetString(args.ApplicationMessage.Payload);
+                  Console.WriteLine(receivedPayload);
+
+
+              };
+
+
+  */
+
+
+
+        }
+    }
+}
+           
+           
+        
+           
+           
